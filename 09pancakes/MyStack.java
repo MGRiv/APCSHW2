370679b3f791298c30a5f2381f2a8ecf26b8f4a0
MGRiv <mgrivera501@gmail.com>
2015-03-22 13:44:48 -0400

    Changed name

---
+++ 09pancakes/MyStack.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class MyStack<T>{
+    public LNode<T> top;
+    public LNode<T> below;
+
+    public boolean empty(){
+	if(top == null){
+	    return true;
+	}
+	return false;
+    }
+    public T peek(){
+	if(empty()){
+	    throw new EmptyStackException();
+	}
+	return top.getValue();
+    }
+    public T pop(){
+	if(empty()){
+	    throw new EmptyStackException();
+	}
+	T temp = top.getValue();
+	top = below;
+	below = below.getNext();
+	return temp;
+    }
+    public T push(T val){
+	LNode<T> n = new LNode<T>(val);
+	n.setNext(top);
+	below = top;
+	top = n;
+	return val;
+    }
+    /*
+    public int search(Object o){
+	//to be implemented later
+    }
+    */
+
+
+}
