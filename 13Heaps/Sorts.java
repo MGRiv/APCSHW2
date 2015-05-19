52c2e4c2fa34e73247adb2aaf7c65197943ea3ca
MGRiv <mgrivera501@gmail.com>
2015-05-18 23:27:52 -0400

    doesn't work

--- 13Heaps/Sorts.java
+++ 13Heaps/Sorts.java
@@ -11,24 +11,24 @@ public class Sorts{
     }
     public static int[] orderheap(int[]q){
 	int[] tem = q;
-	int l = q.length;
+	int l = q.length - 1;
 	while(l > 0){
 	    int which;
-	    if(l % 2 == 0){
-		which = l / 2;
+	    if((l + 1) % 2 == 0){
+		which = (l + 1) / 2;
 	    }else{
-		which = (l - 1) / 2;
+		which = (l) / 2;
 	    }
-	    if(tem[l] > tem[which]){
-		int temp = tem[which];
-		tem[which] = tem[l];
+	    if(tem[l] > tem[which - 1]){
+		int temp = tem[which - 1];
+		tem[which - 1] = tem[l];
 		tem[l] = temp;
 	    }
 	    l--;
 	}
 	return tem;
     }
-    public static int[] orderheap(int[]q){
+    public static int[] organize(int[]q){
 	int[] temp = q;
 	int l = q.length - 1;
 	while(l >= 0){
@@ -37,28 +37,34 @@ public class Sorts{
 	    temp[l] = h;
 	    l--;
 	    int b;
-	    if(l % 2 == 0){
-		b = l / 2;
-	    }else{
+	    if((l + 1) % 2 == 0){
 		b = (l / 2) + 1;
+	    }else{
+		b = (l / 2);
 	    }
 	    int p = 0;
 	    while(p < b){
-		if(temp[p] < temp[(2 * p) + 1] || temp[p] < temp[2 * p]){
-		    if(temp[(2 * p) + 1] < temp[2 * p]){
-			int h = temp[2 * p];
-			temp[2 * p] = temp[p];
+		if(temp[p] < temp[(2 * (p + 1))] || temp[p] < temp[(2 * (p + 1)) - 1]){
+		    if(temp[(2 * (p + 1))] > temp[(2 * (p + 1)) - 1]){
+			h = temp[(2 * (p + 1))];
+			temp[(2 * (p + 1))] = temp[p];
 			temp[p] = h;
-			p = 2 * p;
+			p = 2 * (p+1);
 		    }else{
-			int h = temp[(2 * p) + 1];
-			temp[(2 * p) + 1] = temp[p];
+			h = temp[(2 * (p + 1)) - 1];
+			temp[(2 * (p + 1)) - 1] = temp[p];
 			temp[p] = h;
-			p = (2 * p) + 1;
+			p = (2 * (p + 1)) - 1;
 		    }
 		}
 	    }
 	}
 	return temp;
+    }
+    public static void main(String[]args){
+	int[] a = {6,21,5,8,31,0,76,2,9,12};
+	System.out.println(Arrays.toString(a));
+	heapsort(a);
+	System.out.println(Arrays.toString(a));
     }
 }
